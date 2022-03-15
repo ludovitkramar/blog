ffa16ada3fed858e79e74f857aee8b1592d49ac6
Ludovit-ArchVM <ludovit.34@gmail.com>
2022-03-15 21:22:21 +0800

    some automatic article mapping

---
+++ src/articles/articles.js
@@ -0,0 +1,19 @@
+const articles = [
+"adwaita_theme_for_qt",
+"arch_vm_with_gpu",
+"bash_en_programador_de_tareas",
+"bhyve_wireless_NAT",
+"cant_disable_swap",
+"coturn_synapse",
+"debian_bullseye_bridged_network",
+"grub_rescue_boot",
+"mac_vm",
+"matrix_vm_setup",
+"my_tweaks",
+"nextcloud_snap_setup",
+"plasma_dynamic_wallpaper",
+"resize_root_partition",
+"rsync_commands",
+];
+ 
+ export default articles;
--- src/navigation.js
+++ src/navigation.js
@@ -1,20 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
 import style from './navigation.module.css';
+import articles from './articles/articles.js'
 
 function Nav() {
-    return (
-      <>
-        <nav className={style.nav}>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="q">Article</Link></li>
-            <li><Link to="/qad">Link to nowhere</Link></li>
-            <li><Link to="/Article">nowhere</Link></li>
-          </ul>
-        </nav>
-        <Outlet />
-      </>
-    );
-  }
+  function mapArticles(array) {
+    const links = array.map((e, i) => {
+      return (
+        <li key={i}><Link to={'/article/'+e}>{e}</Link></li>
+      );
+    });
+
+    return links
+  };
+
+  return (
+    <>
+      <nav className={style.nav}>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="q">Article</Link></li>
+          <li><Link to="/qad">Link to nowhere</Link></li>
+          <li><Link to="/Article">nowhere</Link></li>
+          {mapArticles(articles)}
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
 
 export default Nav;
