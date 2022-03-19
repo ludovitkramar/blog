f66cd0224cad966bc57d191354f9c5e3d42320f3
Ludovit-ArchVM <ludovit.34@gmail.com>
2022-03-19 21:13:34 +0800

    kind of better parser?

--- src/article.js
+++ src/article.js
@@ -33,133 +33,120 @@ export default function Article(props) {
 
     console.log(data)
 
-    var graph = [2, 1, 0, 1]; //kantenliste
-    var knoten = { 0: [0], 1: data.split('\n'), };
-    var knoteTypes = { 0: 'root', 1: null, };
-    const spetialChars = ['#', '-', '*', '`', '<', '!'];
+    var G = [1, 0,]; //kantenliste
+    var nD = { 0: 0, };
+    var nT = { 0: 'root', };
 
-    function parse(G, k, kTypes) {
-        k[0][0] += 1; //count rescursions.
-        const knot = k[1] //array of things to be parsed
-        const current = knot[0] //string to be parsed
-        function addNode(content, type) { //content is expected to always be an array
-            const nodeCount = G[0];
-            const newLastNote = nodeCount;
-            const kantenCount = G[1];
-            const currentLastNode = nodeCount - 1;
-            const currentLastNodeContentsCopy = k[currentLastNode];
-            k[currentLastNode] = content
-            kTypes[currentLastNode] = type
-            k[newLastNote] = currentLastNodeContentsCopy.slice(content.length,);
-            kTypes[newLastNote] = null
-            G[0] += 1 //add one to nodes count
-            G[1] += 1 //add one to kanten count
-            G.push(0);
-            G.push(newLastNote)
-            console.log(G)
-            console.log(k)
-            console.log(kTypes)
-        }
-        function complexAddNode(string) { //content is expected to always be an array
-            if (!isNaN(string.split(' ', 1) * 1)) { //if starts with a number
-                var i = 0;
-                while (!isNaN(knot[i].split(' ', 1) * 1) || knot[i].slice(0, 1) == ' ') { //text starts with number or white space
-                    if (i + 1 == knot.lengh) break;
-                    i += 1;
-                }
-                const listArray = knot.slice(0, i) //include in node until i
-                addNode(listArray, 'ol')
-            } else if (string.split(' ', 1) == '-' || string.split(' ', 1) == '*') { //if its an unordered list
-                var i = 0;
-                while (knot[i].split(' ', 1) == '-' || knot[i].split(' ', 1) == '*' || knot[i].slice(0, 1) == ' ') { //text starts with - or * or white space
-                    if (i + 1 == knot.lengh) break;
-                    i += 1;
-                }
-                const listArray = knot.slice(0, i) //include in node until i
-                addNode(listArray, 'ul')
-            } else if (string == '```') { //if it's code block
-                var i = 1;
-                while (knot[i] !== '```') { //text isn't ```
-                    if (i + 1 == knot.lengh) break;
-                    i += 1;
-                }
-                const listArray = knot.slice(0, i + 1) //include in node until i + 1
-                addNode(listArray, 'codeBlock')
+    function parser(markdown, graph, nodeData, nodeType) {
+        const newlineSpetialCharacters = ['# ', '##', '- ', '* ', '``', '!['];
+        var currentParentNode = 0;
+        function getNodesCount() { return graph[0] };
+        function updateNodesLinksCount() {
+            const linksArray = graph.slice(2);
+            var nodesArray = [];
+            const linksCount = linksArray.length / 2;
+            for (const i in linksArray) {
+                const nodeIndex = linksArray[i];
+                if (!nodesArray.includes(nodeIndex)) nodesArray.push(nodeIndex)
             }
+            const nodesCount = nodesArray.length;
+            graph[0] = nodesCount;
+            graph[1] = linksCount;
         }
-        if (current === '') {//if current string is empty delete it from the knoten
-            knot.slice(1,);
-        } else if (spetialChars.indexOf(current[0]) !== -1 || !isNaN(current.split(' ', 1) * 1)) { //if first letter of the string is a spetial character or starts with number followed by dot
-            console.log('Spetial Char in:' + current);
-            switch (current[0]) { //if it is a single line spetial character
-                case '#':
-                    //is a title
-                    addNode([current], 'Title');
-                    break;
-                case '!':
-                    //is an image
-                    addNode([current], 'Image');
-                    break;
-                case '<':
-                    //is <small>
-                    addNode([current], 'Small');
-                    break;
-                default:
-                    complexAddNode(current);
-                    break;
+
+        function isLineImage(line) {
+            var isImage = true;
+            if (!line.slice(0, 2) == '![') isImage = false //if starts correctly
+            //bellow checks order of things
+            if (line.indexOf('!') > line.indexOf('[')) isImage = false;
+            if (line.indexOf('[') > line.indexOf(']')) isImage = false;
+            if (line.indexOf(']') > line.indexOf('(')) isImage = false;
+            if (line.indexOf('(') > line.indexOf('"')) isImage = false;
+            if (line.indexOf('"') > line.lastIndexOf('"')) isImage = false;
+            if (line.lastIndexOf('"') > line.indexOf(')')) isImage = false;
+            if (line.slice(line.indexOf(')')) !== ')') isImage = false; //if there's anything after )
+            return isImage
+        }
+
+        function isLineHeading(beforeFirstWhiteSpace) {
+            var isHeading = true;
+            for (const key in beforeFirstWhiteSpace) {
+                if (beforeFirstWhiteSpace[key] !== '#') isHeading = false;
             }
-        } else { //add normal patagraph
-            console.log('No spetial Char in:' + current);
-            addNode([current], 'Paragraph')
+            return isHeading
         }
 
-        function findParentOf(node) {
-            const array = G.slice(2);
-            for (var i = 1; i < array.length; i += 2) {
-                if (array[i] == node) return array[i - 1];
+        function handleLineWithSpetialCharacters(line) {
+            const beforeFirstWhiteSpace = line.split(' ', 1)[0];
+            if (!isNaN(beforeFirstWhiteSpace * 1)) { //if its a number, ol
+                consumeLine(1); //TODO
+            } else if (isLineImage(line)) {
+                console.warn(line + 'is an image'); //img
+                createNode('Image', line);
+                consumeLine(1);
+            } else if (isLineHeading(beforeFirstWhiteSpace)) {  //heading
+                console.warn(line + 'is heading');
+                createNode('Title', line);
+                consumeLine(1);
+            } else if (beforeFirstWhiteSpace == '-' || beforeFirstWhiteSpace == '*') { //ul
+                consumeLine(1); //TODO
+            } else if (beforeFirstWhiteSpace == '```') {
+                consumeLine(1); //TODO
             }
+            // consumeLine(1)
         }
 
-        function mergeGraphs(cG, ck, ckTypes, node) { //merge with parent of original null {node}
-            const parentNode = findParentOf(node);
-            console.log(`parendNode= ${parentNode}`);
-            k[node] = ck[1];
-            kTypes[node] = ckTypes[1];
-            addNode(ck[2], null);
-
+        function createNode(type, content) {
+            const newNodeNumber = getNodesCount();
+            graph.push(currentParentNode);
+            graph.push(newNodeNumber);
+            nodeData[newNodeNumber] = content;
+            nodeType[newNodeNumber] = type;
+            updateNodesLinksCount();
         }
-        console.log('Recursions: '+ k[0][0])
-        for (const key in kTypes) {
-            if (k[0][0] > 4) { // if there are more than 4 recursions
-                console.warn('Max recursions: '+ k[0][0])
-                break
-            } else if (kTypes[key] == null) { // if there's a node with null type
-                console.log(`found ${k[key]} with null type`);
-                const [childG, childK, childKTypes] = parse([2, 1, 0, 1], { 0: [k[0][0]], 1: k[key] }, { 0: 'root', 1: null, });
-                console.log(childG);
-                console.log(childK);
-                console.log(childKTypes);
-                mergeGraphs(childG, childK, childKTypes, key);
+
+        function consumeLine(quantity) {
+            markdown = markdown.slice(quantity);
+        }
+
+        function processLine(line) {
+            if (line == '') {
+                consumeLine(1)
+                return
             }
+            const firstTwoCharacters = line.slice(0, 2);
+            if (newlineSpetialCharacters.includes(firstTwoCharacters) || !isNaN(firstTwoCharacters * 1) && firstTwoCharacters !== '  ') {
+                handleLineWithSpetialCharacters(line)
+                console.log('spetial chars: ' + line)
+            } else {
+                createNode('Paragraph', line);
+                consumeLine(1);
+            };
         }
 
-        // for (const key in k) {
-        //     console.log(key)
-        //     if (k[key].length > 1) {
-        //         parse([2, 1, 0, 1], { 0: [], 1: k[key] }, {0: 'root', 1: null,})
-        //     } else {
-        //         return
-        //     }
-        // }
+        // for (const line in markdown) 
+        var line = 0
+        while (markdown.length > 0) {
+            console.warn('Progress:');
+            console.log(`line:${line}`);
+            console.log(`line:${markdown[0]}`)
+            console.log(markdown);
+            console.log(graph);
+            console.log(nodeData);
+            console.log(nodeType);
+            processLine(markdown[0]);
+            line += 1;
+        }
 
-        return [G, k, kTypes]
+        return [markdown, graph, nodeData, nodeType]
     }
 
-    const resultArray = parse(graph, knoten, knoteTypes);
+    const [mdsrc, tree, treeContents, treeTags] = parser(data.split('\n'), G, nD, nT);
     console.log('Results:');
-    console.log(resultArray[0]);
-    console.log(resultArray[1]);
-    console.log(resultArray[2]);
+    console.log(mdsrc);
+    console.log(tree);
+    console.log(treeContents);
+    console.log(treeTags);
 
     return (
         <article className={style.article}>
